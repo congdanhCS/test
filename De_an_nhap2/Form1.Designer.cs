61bb883f9773a980e50033f1d79f5eda39d1c4f8
congdanhCS <nguyencongdanh2005.anhuu@gmail.com>
2024-05-02 18:31:22 +0700

    init project

---
+++ De_an_nhap2/Form1.Designer.cs
@@ -0,0 +1,373 @@
+﻿namespace De_an_nhap2
+{
+    partial class formTHN
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            RodA = new PictureBox();
+            label1 = new Label();
+            label2 = new Label();
+            RodB = new PictureBox();
+            label3 = new Label();
+            RodC = new PictureBox();
+            pic1 = new PictureBox();
+            pic2 = new PictureBox();
+            pic3 = new PictureBox();
+            pic4 = new PictureBox();
+            pic5 = new PictureBox();
+            pic6 = new PictureBox();
+            pic7 = new PictureBox();
+            pic8 = new PictureBox();
+            lblTimer = new Label();
+            Timer = new System.Windows.Forms.Timer(components);
+            ButtonStart = new Button();
+            ButtonStop = new Button();
+            ButtonNote = new Button();
+            LabelDisk = new Label();
+            Level = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)RodA).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)RodB).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)RodC).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pic8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)Level).BeginInit();
+            SuspendLayout();
+            // 
+            // RodA
+            // 
+            RodA.Image = Properties.Resources.Screenshot_2024_04_16_123941;
+            RodA.Location = new Point(65, 258);
+            RodA.Name = "RodA";
+            RodA.Size = new Size(280, 260);
+            RodA.SizeMode = PictureBoxSizeMode.CenterImage;
+            RodA.TabIndex = 0;
+            RodA.TabStop = false;
+            RodA.Click += Rod_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(194, 521);
+            label1.Name = "label1";
+            label1.Size = new Size(24, 23);
+            label1.TabIndex = 1;
+            label1.Text = "A";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(481, 521);
+            label2.Name = "label2";
+            label2.Size = new Size(23, 23);
+            label2.TabIndex = 3;
+            label2.Text = "B";
+            // 
+            // RodB
+            // 
+            RodB.Image = Properties.Resources.Screenshot_2024_04_16_123941;
+            RodB.Location = new Point(351, 258);
+            RodB.Name = "RodB";
+            RodB.Size = new Size(280, 260);
+            RodB.SizeMode = PictureBoxSizeMode.CenterImage;
+            RodB.TabIndex = 2;
+            RodB.TabStop = false;
+            RodB.Click += Rod_Click;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Times New Roman", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(766, 521);
+            label3.Name = "label3";
+            label3.Size = new Size(24, 23);
+            label3.TabIndex = 5;
+            label3.Text = "C";
+            // 
+            // RodC
+            // 
+            RodC.Image = Properties.Resources.Screenshot_2024_04_16_123941;
+            RodC.Location = new Point(637, 258);
+            RodC.Name = "RodC";
+            RodC.Size = new Size(280, 260);
+            RodC.SizeMode = PictureBoxSizeMode.CenterImage;
+            RodC.TabIndex = 4;
+            RodC.TabStop = false;
+            RodC.Click += Rod_Click;
+            // 
+            // pic1
+            // 
+            pic1.Image = Properties.Resources.Screenshot_2024_04_15_124358;
+            pic1.Location = new Point(65, 484);
+            pic1.Name = "pic1";
+            pic1.Size = new Size(280, 20);
+            pic1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic1.TabIndex = 6;
+            pic1.TabStop = false;
+            pic1.Tag = "1";
+            pic1.Visible = false;
+            // 
+            // pic2
+            // 
+            pic2.Image = Properties.Resources.Screenshot_2024_04_15_124431;
+            pic2.Location = new Point(351, 484);
+            pic2.Name = "pic2";
+            pic2.Size = new Size(280, 20);
+            pic2.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic2.TabIndex = 7;
+            pic2.TabStop = false;
+            pic2.Tag = "2";
+            pic2.Visible = false;
+            pic2.Click += Rod_Click;
+            // 
+            // pic3
+            // 
+            pic3.Image = Properties.Resources.Screenshot_2024_04_15_124443;
+            pic3.Location = new Point(637, 354);
+            pic3.Name = "pic3";
+            pic3.Size = new Size(280, 20);
+            pic3.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic3.TabIndex = 8;
+            pic3.TabStop = false;
+            pic3.Tag = "3";
+            pic3.Visible = false;
+            // 
+            // pic4
+            // 
+            pic4.Image = Properties.Resources.Screenshot_2024_04_15_124452;
+            pic4.Location = new Point(637, 380);
+            pic4.Name = "pic4";
+            pic4.Size = new Size(280, 20);
+            pic4.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic4.TabIndex = 9;
+            pic4.TabStop = false;
+            pic4.Tag = "4";
+            pic4.Visible = false;
+            // 
+            // pic5
+            // 
+            pic5.Image = Properties.Resources.Screenshot_2024_04_15_124459;
+            pic5.Location = new Point(637, 406);
+            pic5.Name = "pic5";
+            pic5.Size = new Size(280, 20);
+            pic5.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic5.TabIndex = 10;
+            pic5.TabStop = false;
+            pic5.Tag = "5";
+            pic5.Visible = false;
+            // 
+            // pic6
+            // 
+            pic6.Image = Properties.Resources.Screenshot_2024_04_15_124508;
+            pic6.Location = new Point(637, 444);
+            pic6.Name = "pic6";
+            pic6.Size = new Size(280, 20);
+            pic6.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic6.TabIndex = 11;
+            pic6.TabStop = false;
+            pic6.Tag = "6";
+            pic6.Visible = false;
+            // 
+            // pic7
+            // 
+            pic7.Image = Properties.Resources.Screenshot_2024_04_16_125838;
+            pic7.Location = new Point(637, 464);
+            pic7.Name = "pic7";
+            pic7.Size = new Size(280, 20);
+            pic7.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic7.TabIndex = 12;
+            pic7.TabStop = false;
+            pic7.Tag = "7";
+            pic7.Visible = false;
+            // 
+            // pic8
+            // 
+            pic8.Image = Properties.Resources.Screenshot_2024_04_16_122128;
+            pic8.Location = new Point(637, 484);
+            pic8.Name = "pic8";
+            pic8.Size = new Size(280, 20);
+            pic8.SizeMode = PictureBoxSizeMode.CenterImage;
+            pic8.TabIndex = 13;
+            pic8.TabStop = false;
+            pic8.Tag = "8";
+            pic8.Visible = false;
+            // 
+            // lblTimer
+            // 
+            lblTimer.AutoSize = true;
+            lblTimer.BackColor = Color.Yellow;
+            lblTimer.BorderStyle = BorderStyle.Fixed3D;
+            lblTimer.Font = new Font("Times New Roman", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            lblTimer.Location = new Point(12, 9);
+            lblTimer.Name = "lblTimer";
+            lblTimer.Size = new Size(140, 25);
+            lblTimer.TabIndex = 14;
+            lblTimer.Text = "Time: 00:00:00";
+            // 
+            // Timer
+            // 
+            Timer.Interval = 1000;
+            Timer.Tick += Timer_Tick;
+            // 
+            // ButtonStart
+            // 
+            ButtonStart.BackColor = Color.Cyan;
+            ButtonStart.Location = new Point(274, 12);
+            ButtonStart.Name = "ButtonStart";
+            ButtonStart.Size = new Size(121, 29);
+            ButtonStart.TabIndex = 15;
+            ButtonStart.Text = "Start";
+            ButtonStart.UseVisualStyleBackColor = false;
+            ButtonStart.Click += ButtonStart_Click;
+            // 
+            // ButtonStop
+            // 
+            ButtonStop.BackColor = Color.Cyan;
+            ButtonStop.Enabled = false;
+            ButtonStop.Location = new Point(442, 12);
+            ButtonStop.Name = "ButtonStop";
+            ButtonStop.Size = new Size(94, 29);
+            ButtonStop.TabIndex = 16;
+            ButtonStop.Text = "Stop";
+            ButtonStop.UseVisualStyleBackColor = false;
+            ButtonStop.Click += ButtonStop_Click;
+            // 
+            // ButtonNote
+            // 
+            ButtonNote.BackColor = Color.Cyan;
+            ButtonNote.Location = new Point(590, 12);
+            ButtonNote.Name = "ButtonNote";
+            ButtonNote.Size = new Size(94, 29);
+            ButtonNote.TabIndex = 17;
+            ButtonNote.Text = "Note";
+            ButtonNote.UseVisualStyleBackColor = false;
+            ButtonNote.Click += ButtonNote_Click;
+            // 
+            // LabelDisk
+            // 
+            LabelDisk.AutoSize = true;
+            LabelDisk.BackColor = Color.Cyan;
+            LabelDisk.Location = new Point(334, 82);
+            LabelDisk.Name = "LabelDisk";
+            LabelDisk.Size = new Size(61, 22);
+            LabelDisk.TabIndex = 18;
+            LabelDisk.Text = "Level:";
+            // 
+            // Level
+            // 
+            Level.Location = new Point(401, 82);
+            Level.Maximum = new decimal(new int[] { 8, 0, 0, 0 });
+            Level.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            Level.Name = "Level";
+            Level.Size = new Size(57, 30);
+            Level.TabIndex = 19;
+            Level.Tag = "";
+            Level.Value = new decimal(new int[] { 3, 0, 0, 0 });
+            // 
+            // formTHN
+            // 
+            AutoScaleDimensions = new SizeF(11F, 22F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(982, 553);
+            Controls.Add(Level);
+            Controls.Add(LabelDisk);
+            Controls.Add(ButtonNote);
+            Controls.Add(ButtonStop);
+            Controls.Add(ButtonStart);
+            Controls.Add(lblTimer);
+            Controls.Add(pic8);
+            Controls.Add(pic7);
+            Controls.Add(pic6);
+            Controls.Add(pic5);
+            Controls.Add(pic4);
+            Controls.Add(pic3);
+            Controls.Add(pic2);
+            Controls.Add(pic1);
+            Controls.Add(label3);
+            Controls.Add(RodC);
+            Controls.Add(label2);
+            Controls.Add(RodB);
+            Controls.Add(label1);
+            Controls.Add(RodA);
+            DoubleBuffered = true;
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.Fixed3D;
+            Margin = new Padding(4, 3, 4, 3);
+            MaximizeBox = false;
+            Name = "formTHN";
+            ShowIcon = false;
+            Text = "HanoiTower";
+            TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)RodA).EndInit();
+            ((System.ComponentModel.ISupportInitialize)RodB).EndInit();
+            ((System.ComponentModel.ISupportInitialize)RodC).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pic8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)Level).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox RodA;
+        private Label label1;
+        private Label label2;
+        private PictureBox RodB;
+        private Label label3;
+        private PictureBox RodC;
+        private PictureBox pic1;
+        private PictureBox pic2;
+        private PictureBox pic3;
+        private PictureBox pic4;
+        private PictureBox pic5;
+        private PictureBox pic6;
+        private PictureBox pic7;
+        private PictureBox pic8;
+        private Label lblTimer;
+        private System.Windows.Forms.Timer Timer;
+        private Button ButtonStart;
+        private Button ButtonStop;
+        private Button ButtonNote;
+        private Label LabelDisk;
+        private NumericUpDown Level;
+    }
+}
---
+++ De_an_nhap2/Form1.cs
@@ -0,0 +1,204 @@
+﻿using System.ComponentModel.Design;
+
+namespace De_an_nhap2
+{
+    public partial class formTHN : System.Windows.Forms.Form
+    {
+        TimeSpan time;
+        PictureBox[] Disks;
+        StackRod<PictureBox> DisksA, DisksB, DisksC, FirstClickDisks, SecondClickDisks;
+        const int Y1 = 484, DiskHeight = 20;
+        public formTHN()
+        {
+            InitializeComponent();
+            Disks = new PictureBox[] { pic1, pic2, pic3, pic4, pic5, pic6, pic7, pic8 };
+            RodA.Tag = DisksA = new StackRod<PictureBox>();
+            RodB.Tag = DisksB = new StackRod<PictureBox>();
+            RodC.Tag = DisksC = new StackRod<PictureBox>();
+        }
+
+        private void ButtonNote_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Di chuyển các đĩa từ cọc A qua cọc C bằng cách nhấp chuột trái vào cọc tương ứng, " +
+                "sao cho tất cả các đĩa xếp đúng thứ tự ban đầu." + "\nLưu ý: \n-Mỗi lần di chuyển chỉ được di chuyển 1 đĩa." +
+                "\n-Đĩa trên cùng phải nhỏ hơn đĩa nằm phía dưới (nếu có) của mỗi cọc.", "Lưu ý", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            time = time.Add(new TimeSpan(0, 0, 1));
+            lblTimer.Text = time.ToString();
+            lblTimer.Text = string.Format("Time: {0:00}:{1:00}:{2:00}", time.Hours, time.Minutes, time.Seconds);
+        }
+
+        private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            Timer.Stop();
+            ButtonStop.Enabled = false;
+            ButtonStart.Enabled = true;
+            ButtonStart.Text = "PlayAgain";
+            Level.Enabled = true;
+        }
+
+        private void ButtonStart_Click(object sender, EventArgs e)
+        {
+            //Reset
+            Timer.Stop();
+            foreach (PictureBox D in Disks)
+            {
+                D.Visible = false;
+            }
+            time = new TimeSpan(0);
+            lblTimer.Text = "Time: 00:00:00";
+            DisksA.Clear(); DisksB.Clear(); DisksC.Clear();
+            RodA.BorderStyle = RodB.BorderStyle = RodC.BorderStyle = BorderStyle.None;
+            FirstClickDisks = SecondClickDisks = null;
+            //Initialze
+            Level.Enabled = false;
+            ButtonStop.Enabled = true;
+            ButtonStart.Text = "PlayAgain";
+            ButtonStart.Enabled = false;
+            int x = RodA.Location.X, y = Y1;
+            for (int i = (int)Level.Value - 1; i >= 0; i--, y = y - DiskHeight)
+            {
+                Disks[i].Location = new Point(x, y);
+                Disks[i].Visible = true;
+                DisksA.Push(Disks[i]);
+            }
+            Timer.Start();
+        }
+        private void Rod_Click(object sender, EventArgs e)
+        {
+            if (Level.Enabled) return;
+            PictureBox ClickRod = (PictureBox)sender;
+            StackRod<PictureBox> DiskOfClickRod = (StackRod<PictureBox>)ClickRod.Tag;
+
+            if (FirstClickDisks == null)
+            {
+                if (DiskOfClickRod.IsEmpty()) return;
+                else
+                {
+                    FirstClickDisks = DiskOfClickRod;
+                    ClickRod.BorderStyle = BorderStyle.FixedSingle;
+                }
+            }
+            else if (SecondClickDisks == null)
+            {
+                if (DiskOfClickRod == FirstClickDisks)
+                {
+                    FirstClickDisks = null;
+                    ClickRod.BorderStyle = BorderStyle.None;
+                    return;
+                }
+                SecondClickDisks = DiskOfClickRod;
+                ProcessMovingDisk(ClickRod);
+            }
+        }
+
+        private void ProcessMovingDisk(PictureBox clickRod)
+        {
+            if (SecondClickDisks.IsEmpty())
+                MoveDisk(new Point(clickRod.Location.X, Y1));
+            else
+            {
+                PictureBox FirstTopDisk = FirstClickDisks.Peek();
+                PictureBox SecondTopDisk = SecondClickDisks.Peek();
+                if (int.Parse(FirstTopDisk.Tag.ToString()) < int.Parse(SecondTopDisk.Tag.ToString()))
+                {
+                    MoveDisk(new Point(SecondTopDisk.Location.X, SecondTopDisk.Location.Y - DiskHeight));
+                }
+                else
+                {
+                    SecondClickDisks = null;
+                }
+            }
+        }
+        private void MoveDisk(Point point)
+        {
+            PictureBox FirstTopDisk = FirstClickDisks.Pop();
+            FirstTopDisk.Location = point;
+            SecondClickDisks.Push(FirstTopDisk);
+            FirstClickDisks = SecondClickDisks = null;
+            RodA.BorderStyle = RodB.BorderStyle = RodC.BorderStyle = BorderStyle.None;
+            if (DisksC.Count() == Level.Value)
+            {
+                ButtonStop.PerformClick();
+                MessageBox.Show("Amazing Goodjob:>", "CONGRATULATIONS!!!!!!", MessageBoxButtons.OK);
+            }
+        }
+    }
+    public class Node<T>
+    {
+        public Node<T> Next;
+        public T Data;
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+    public class StackRod<T>
+    {
+        private Node<T> top;
+        public void Push(T element)
+        {
+            Node<T> newNode = new Node<T>(element);
+            newNode.Next = top;
+            top = newNode;
+        }
+        public T Pop()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            T tempData = top.Data;
+            top = top.Next;
+            return tempData;
+        }
+        public bool IsEmpty()
+        {
+            return top == null;
+        }
+        public void Clear()
+        {
+            top = null;
+        }
+        public T Peek()
+        {
+            if (top == null)
+            {
+                throw new InvalidOperationException("Stack is empty");
+            }
+            return top.Data;
+        }
+        public int Count()
+        {
+            int count = 0;
+            Node<T> current = top;
+            while (current != null)
+            {
+                count++;
+                current = current.Next;
+            }
+            return count;
+        }
+
+        public static explicit operator StackRod<T>(PictureBox v)
+        {
+            StackRod<T> stack = new StackRod<T>();
+            // Thêm các đối tượng từ PictureBox vào stack
+            // Ở đây, tôi giả sử rằng các đối tượng trong PictureBox.Tag là các đối tượng cần push vào stack
+            if (v.Tag != null && v.Tag is IEnumerable<T>)
+            {
+                IEnumerable<T> items = (IEnumerable<T>)v.Tag;
+                foreach (T item in items)
+                {
+                    stack.Push(item);
+                }
+            }
+            return stack;
+        }
+    }
+}
